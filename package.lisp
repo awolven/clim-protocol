1fc787fcc5510154a607bd33f0aa7b820ca1d781
Andrew Wolven <awolven@gmail.com>
2024-01-22 21:14:35 -0600

    exports

--- package.lisp
+++ package.lisp
@@ -5,10 +5,8 @@
 	   #:medium
 	   #:port
 	   #:mirrored-sheet-mixin
-	   #:presentation
-	   #:presentation-object
-	   #:presentation-type
 	   #:handle-event
+	   #:event
 	   #:device-event
 	   #:keyboard-event
 	   #:key-press-event
@@ -33,8 +31,94 @@
 	   #:pointer-event-native-x
 	   #:pointer-event-native-y
 	   #:pointer-event-pointer
-	   #:accept))
 
-(defpackage :clim-internals
-  (:use :clim :common-lisp))
+
+	   ;; presentation types
+	   #:accept
+	   #:accept-from-string
+	   #:accept-1
+	   #:accept-method
+	   #:accept-present-default
+	   #:accept-present-default-method
+	   #:apply-presentation-generic-function
+	   #:default-describe-presentation-type
+	   #:define-default-presentation-method
+	   #:define-presentation-generic-function
+	   #:define-presentation-method
+	   #:define-presentation-translator
+	   #:define-presentation-type
+	   #:define-presentation-type-abbreviation
+	   #:describe-presentation-type
+	   #:describe-presentation-type-method
+	   #:expand-presentation-type-abbreviation
+	   #:expand-presentation-type-abbreviation-1
+	   #:expression
+	   #:find-applicable-translators
+	   #:find-presentation-translators
+	   #:find-presentation-type-class
+	   #:form
+	   #:funcall-presentation-generic-function
+	   #:highlight-applicable-presentation
+	   #:highlight-presentation
+	   #:highlight-presentation-method
+	   #:input-context-type
+	   #:make-presentation-type-specifier
+	   #:map-over-presentation-type-supertypes
+	   #:map-over-presentation-type-supertypes-method
+	   #:present
+	   #:present-method
+	   #:present-to-string
+	   #:presentation
+	   #:presentation-default-preprocessor
+	   #:presentation-default-preprocessor-method
+	   #:presentation-matches-context-type
+	   #:presentation-modifier
+	   #:presentation-object
+	   #:presentation-subtypep
+	   #:presentation-subtypep-method
+	   #:presentation-type
+	   #:presentation-type-direct-supertypes
+	   #:presentation-type-history
+	   #:presentation-type-history-method
+	   #:presentation-type-of
+	   #:presentation-type-name
+	   #:presentation-type-options
+	   #:presentation-type-parameters
+	   #:presentation-type-specifier-p
+	   #:presentation-type-specifier-p-method
+	   #:presentation-typep
+	   #:presentation-typep-method
+	   #:presentationp
+	   #:prompt-for-accept
+	   #:prompt-for-accept-1
+	   #:sequence-enumerated
+	   #:standard-presentation
+	   #:stream-accept
+	   #:stream-present
+	   #:subset
+	   #:subset-alist
+	   #:subset-completion
+	   #:subset-sequence
+	   #:unhighlight-highlighted-presentation
+	   #:with-input-context
+	   #:with-output-as-presentation
+	   #:with-presentation-type-decoded
+	   #:with-presentation-type-options
+	   #:with-presentation-type-parameters
+	   #:with-presentations-to-string
+
+	   ;; application-frames
+	   #:*application-frame*
+	   #:application-frame
+	   #:application-frame-p
+	   #:default-frame-top-level
+	   #:find-application-frame
+	   #:find-frame-manager
+	   #:frame-manager))
+	   
+	   
+
+	   
+
+
 
